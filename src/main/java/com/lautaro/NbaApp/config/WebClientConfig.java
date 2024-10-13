37f28b4920ab0a1ef973277de72578fceff1e342
LautaroUnlp <kevin.soto194341@alumnos.info.unlp.edu.ar>
2024-10-12 23:54:59 -0300

    actualization of the webClient.java

--- src/main/java/com/lautaro/NbaApp/config/WebClientConfig.java
+++ src/main/java/com/lautaro/NbaApp/config/WebClientConfig.java
@@ -11,8 +11,7 @@ public class WebClientConfig {
     public WebClient webClient (WebClient.Builder builder) {
         return builder
                 .baseUrl("https://nba-stats4.p.rapidapi.com") // URL base para la API
-                .defaultHeader("x-rapidapi-key", "238c8f4ec8mshfd0c9d4a35b3bbfp1527bcjsn9736c4541a87") // Llave API
-                .defaultHeader("x-rapidapi-host", "nba-stats4.p.rapidapi.com") // Host de la API
+                .defaultHeader("Authorization", "13b0929f-bc00-42f8-ad34-946ebbcd38cb") // Llave API
                 .build();
     }
 }
